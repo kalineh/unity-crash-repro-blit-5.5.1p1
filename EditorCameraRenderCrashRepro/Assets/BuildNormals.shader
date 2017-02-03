d8ba934e291989e05862f737e7c1ad9089c8b8a0
kalineh <kalin.eh@gmail.com>
2017-02-03 13:47:28 -0800

    initial upload

---
+++ EditorCameraRenderCrashRepro/Assets/BuildNormals.shader
@@ -0,0 +1,70 @@
+﻿Shader "CosmicTrip/BuildNormals"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+
+			float4 _MainTex_TexelSize;
+			sampler2D _MainTex;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+
+				float delta = _MainTex_TexelSize.x; 
+				float left = tex2D(_MainTex, i.uv - float2(delta, 0)).x;
+				float right = tex2D(_MainTex, i.uv + float2(delta, 0)).x;
+				float bottom = tex2D(_MainTex, i.uv - float2(0, delta)).x;
+				float top = tex2D(_MainTex, i.uv + float2(0, delta)).x;
+				
+				float x = (right - left) / (2.0 * delta); 
+				float y = (top - bottom) / (2.0 * delta); 
+				float z = (1.0 - (x * x) - (y * y)); 
+
+				fixed4 packedNormals = fixed4(x, y, z, 0); 
+
+				// normalize between 0 and 1 (range is -1 to 1) 
+				packedNormals = (packedNormals + 1.0) / 2.0; 
+
+				// might as well include copy of depth buffer 
+				fixed4 col = tex2D(_MainTex, i.uv);
+				packedNormals.w = col.x; 
+
+				return packedNormals; 
+			}
+			ENDCG
+		}
+	}
+}
---
+++ EditorCameraRenderCrashRepro/Assets/MapCamera.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(MapCamera))]
+public class MapCameraEditor
+    : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Refresh"))
+        {
+            var self = target as MapCamera;
+            self.EditorRefreshMap();
+        }
+    }
+}
+
+#endif
+
+//[ExecuteInEditMode]
+public class MapCamera : MonoBehaviour {
+    
+    public Camera MapCam;
+    [SerializeField] private RenderTexture LinearDepth;
+    [SerializeField] private RenderTexture Texture;
+    [SerializeField] private RenderTexture Depth;
+    [SerializeField] private RenderTexture TempB;
+    [SerializeField] private RenderTexture DepthNormals;
+
+    public static MapCamera Instance;
+    [SerializeField] private Material BlurMat;
+    [SerializeField] private Material LinearMat;
+    [SerializeField] private Material BuildNormalsMat;
+    public int Count = 4; 
+
+    public void EditorRefreshMap()
+    {
+        OnScriptReload();
+    }
+
+	void Start()
+    {
+        OnScriptReload();
+    }
+
+    void OnScriptReload()
+    {
+        Instance = this;
+
+#if UNITY_EDITOR
+        if (UnityEngine.Application.isPlaying)
+        {
+        }
+#else
+        if (!GameSettings.Instance.EnableCommandCenter)
+        {
+            MapCam.enabled = false;
+            return; 
+        }
+#endif
+
+        MapCam.enabled = true;
+        MapCam.depthTextureMode = DepthTextureMode.DepthNormals;
+        MapCam.SetTargetBuffers(Texture.colorBuffer, Depth.depthBuffer);
+        MapCam.Render();
+        MapCam.enabled = false;
+
+        // normalize depth 
+        LinearMat.SetFloat("_ZNear", MapCam.nearClipPlane);
+        LinearMat.SetFloat("ZFar", MapCam.farClipPlane);
+        Graphics.Blit(Depth, LinearDepth, LinearMat); 
+
+        // passthru 
+        BlurMat.SetFloat("_deltaX", 0);
+        BlurMat.SetFloat("_deltaY", 0); 
+        Graphics.Blit(LinearDepth, Texture, BlurMat);
+
+        // build normals 
+        Graphics.Blit(Texture, DepthNormals, BuildNormalsMat);
+
+        // blur 
+        BlurMat.SetFloat("_deltaX", 1);
+        BlurMat.SetFloat("_deltaY", 0);
+        for (var i = 0; i < Count; ++i)
+        {
+            Graphics.Blit(Texture, TempB, BlurMat);
+            Graphics.Blit(TempB, Texture, BlurMat);
+
+            Graphics.Blit(DepthNormals, TempB, BlurMat);
+            Graphics.Blit(TempB, DepthNormals, BlurMat);
+        }
+
+        BlurMat.SetFloat("_deltaX", 0);
+        BlurMat.SetFloat("_deltaY", 1);
+        for (var i = 0; i < Count; ++i)
+        {
+            Graphics.Blit(Texture, TempB, BlurMat);
+            Graphics.Blit(TempB, Texture, BlurMat);
+
+            Graphics.Blit(DepthNormals, TempB, BlurMat);
+            Graphics.Blit(TempB, DepthNormals, BlurMat);
+        }
+
+        // free up unused textures 
+        TempB.Release();
+        Depth.Release();
+        LinearDepth.Release(); 
+    }
+}
